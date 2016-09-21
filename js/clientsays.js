934a627697666cbccbce48340b3a75e7f7190644
Marko Tomic <markotomic.96@hotmail.com>
2016-09-21 17:30:16 +0200

    First Commit

---
+++ js/clientsays.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$("#profession").html("<h3 class='cs_name'>fank sims</h3> <p class='cs_underscore'>___</p><br><p class='cs_proffesion'>photographer</p>");
+		$("#description").html("<p id='description'>Sed ut perspiciatis unde omnis iste natus error sit<br> voluptatem accusantium doloremque laudantium,<br> totam rem aperiam, eaque ipsa quae ab illo inventore<br> veritatis et quasi architecto beatae vitae dicta sunt<br> explicabo.</p>");
+	$(".one").click(function(){
+		$("#profession").html("<h3 class='cs_name'>fank sims</h3> <p class='cs_underscore'>___</p><br><p class='cs_proffesion'>photographer</p>");
+		$("#description").html("<p id='description'>Sed ut perspiciatis unde omnis iste natus error sit<br> voluptatem accusantium doloremque laudantium,<br> totam rem aperiam, eaque ipsa quae ab illo inventore<br> veritatis et quasi architecto beatae vitae dicta sunt<br> explicabo.</p>");
+	});
+	$(".two").click(function(){
+		$("#profession").html("<h3 class='cs_name'>mio karalejic</h3> <p class='cs_underscore'>___</p><br><p class='cs_proffesion'>designer</p>");
+		$("#description").html("<p id='description'>Unde omnis iste natus error sit<br> accusantium voluptatem doloremque laudantium,<br> totam rem aperiam, eaque ipsa quae ab illo inventore<br> veritatis et quasi architecto beatae vitae.</p>");
+	});
+	$(".three").click(function(){
+		$("#profession").html("<h3 class='cs_name'>radisha trajkovic</h3> <p class='cs_underscore'>___</p><br><p class='cs_proffesion'>singer</p>");
+		$("#description").html("<p id='description'>Iste unde omnis iste natus error sit acivolus<br> voluptatem doloremque laudantium aperiam,<br> totam rem aperiam, eaque ipsa architecto quae ab illo inventore<br> veritatis et quasi architecto beatae vitae inventore.</p>");
+	});
+	$(".four").click(function(){
+		$("#profession").html("<h3 class='cs_name'>dragi domic</h3> <p class='cs_underscore'>___</p><br><p class='cs_proffesion'>singer</p>");
+		$("#description").html("<p id='description'>Perspiciatis sit unde omnis iste natus error sit acivolus voluptatem<br> doloremque laudantium aperiam totam voluptatem illo ipsa,<br> totam rem aperiam, eaque ipsa architecto quae ab illo inventore<br> veritatis et quasi architecto beatae explicabo.</p>");
+	});
+});
